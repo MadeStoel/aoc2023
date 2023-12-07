a88d099f8b83c150c709400f3643224ccbd1575c
Maarten Stoelhorst <maarten.stoelhorst@mybit.nl>
2023-12-07 12:28:13 +0100

    Day 7 part 2

---
+++ day7/part2.js
@@ -0,0 +1,137 @@
+const fs = require("fs");
+
+const input = fs.readFileSync(__dirname + "/input.txt", "utf8").split("\n").filter(Boolean);
+
+const hands = categorizeHands();
+
+sortHands(hands);
+
+const singleArrayOfHands = Object.values(hands).reduce((result, categorizedHands) => {
+    result.push(...categorizedHands);
+    return result;
+}, []);
+
+console.log(singleArrayOfHands.reduce((answer, hand, index) => answer + (hand.bid * (index + 1)), 0));
+
+function sortHands(allHands) {
+    Object.values(allHands).forEach(categorizedHands => {
+        categorizedHands.sort((a, b) => {
+            const firstCards = a.hand.split('');
+            const secondCards = b.hand.split('');
+
+            for (let i = 0; i < firstCards.length; i++) {
+                if (firstCards[i] !== secondCards[i]) {
+                    return getCardStrength(firstCards[i]) > getCardStrength(secondCards[i]) ? 1 : -1;
+                }
+            }
+        });
+    });
+}
+
+function categorizeHands() {
+    return input.reduce((result, row) => {
+        const [hand, bid] = row.split(' ');
+        const handType = getHandType(hand);
+        const handTypeStrength = getHandTypeStrength(handType);
+
+        const parsedHand = {
+            hand,
+            handType,
+            bid: +bid
+        };
+
+        !!result[handTypeStrength] ? result[handTypeStrength].push(parsedHand) : result[handTypeStrength] = [parsedHand];
+
+        return result;
+    }, {});
+}
+
+function getHandType(hand) {
+    const occurrencesMap = {};
+    const handArrayWithoutJokers = hand.split('').filter((card) => card !== 'J');
+    const jokerAmount = 5 - handArrayWithoutJokers.length;
+
+    handArrayWithoutJokers.forEach(card => {
+        !occurrencesMap[card] ? occurrencesMap[card] = 1 : occurrencesMap[card]++;
+    });
+
+    let mostCommonCard;
+
+    try {
+        mostCommonCard = Object.entries(occurrencesMap).reduce((result, map) => {
+            return map[1] > result[1] ? map : result
+        });
+    } catch (_) {
+        mostCommonCard = null;
+    }
+
+    if (!!mostCommonCard) {
+        occurrencesMap[mostCommonCard[0]] += jokerAmount;
+    } else {
+        occurrencesMap['J'] = 5;
+    }
+
+    const occurrences = Object.values(occurrencesMap);
+
+    if (occurrences.includes(5)) return 'five-of-a-kind';
+    if (occurrences.includes(4)) return 'four-of-a-kind';
+    if (occurrences.includes(3)) {
+        return occurrences.includes(2) ? 'full-house' : 'three-of-a-kind';
+    }
+    if (occurrences.includes(2)) {
+        const pairAmounts = occurrences.filter((occurrence) => occurrence === 2).length;
+        return pairAmounts === 2 ? 'two-pair' : 'one-pair';
+    }
+
+    return 'high-card';
+}
+
+function getHandTypeStrength(handType) {
+    switch (handType) {
+        case 'high-card':
+            return 1;
+        case 'one-pair':
+            return 2;
+        case 'two-pair':
+            return 3;
+        case 'three-of-a-kind':
+            return 4;
+        case 'full-house':
+            return 5;
+        case 'four-of-a-kind':
+            return 6;
+        case 'five-of-a-kind':
+            return 7;
+    }
+}
+
+function getCardStrength(card) {
+    switch (card) {
+        case '2':
+            return 2;
+        case '3':
+            return 3;
+        case '4':
+            return 4;
+        case '5':
+            return 5;
+        case '6':
+            return 6;
+        case '7':
+            return 7;
+        case '8':
+            return 8;
+        case '9':
+            return 9;
+        case 'T':
+            return 10;
+        case 'J':
+            return 1;
+        case 'Q':
+            return 12;
+        case 'K':
+            return 13;
+        case 'A':
+            return 14;
+    }
+}
