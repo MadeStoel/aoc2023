e102756313f5dd07d94dab5e795ca328b9d3bf5e
Maarten Stoelhorst <maarten.stoelhorst@mybit.nl>
2023-12-18 11:43:53 +0100

    WIP

--- day17/part1.js
+++ day17/part1.js
@@ -1,15 +1,15 @@
 const fs = require("fs");
 
 let input = fs.readFileSync(__dirname + "/input.txt", "utf8").split("\n")
-              .filter(Boolean)
-              .map(row => row.split("")
-                             .map(number => +number)
-                             .filter(Boolean)
-              );
+    .filter(Boolean)
+    .map(row => row.split("")
+        .map(number => +number)
+        .filter(Boolean)
+    );
 
-const goalBlockKey  = `${ input[ 0 ].length - 1 }-${ input.length - 1 }`;
-const maxX          = input[ 0 ].length - 1;
-const maxY          = input.length - 1;
+const goalBlockKey = `${input[0].length - 1}-${input.length - 1}`;
+const maxX = input[0].length - 1;
+const maxY = input.length - 1;
 const checkedBlocks = [];
 
 const blocks = {
@@ -20,55 +20,83 @@ const blocks = {
         y: 0
     }
 };
-//
-// while ( checkedBlocks.length !== (maxX + 1) * (maxY + 1)) {
-//     const lowestBlock = getLowestHeatLossBlock();
-//
-//     stepSouth(lowestBlock);
-// }
 
-for ( let i = 0; i < 5; i++ ) {
+while (!checkedBlocks.includes(goalBlockKey)) {
     const lowestBlock = getLowestHeatLossBlock();
 
-    stepSouth(lowestBlock);
+    const availableDirections = ['north',
+        'east',
+        'south',
+        'west'];
 
-    checkedBlocks.push(`${ lowestBlock.x }-${ lowestBlock.y }`);
+    availableDirections.forEach(direction => {
+        step(lowestBlock, direction);
+    });
+
+    checkedBlocks.push(`${lowestBlock.x}-${lowestBlock.y}`);
 }
 
-console.log(blocks);
+// ===============DEBUGGING=====================
+let blockToDraw = blocks[goalBlockKey]
 
-function stepSouth(lowestBlock) {
-    const nextBlockY = lowestBlock.y + 1;
+while (blockToDraw.heatLoss !== 0) {
+    input[blockToDraw.y][blockToDraw.x] = '#';
 
-    if ( nextBlockY > maxY ) return;
+    blockToDraw = blocks[blockToDraw.fromBlock];
+}
 
-    const nextBlockX           = lowestBlock.x;
-    const lowestBlockKey       = `${ lowestBlock.x }-${ lowestBlock.y }`;
-    const nextBlockKey         = `${ nextBlockX }-${ nextBlockY }`;
-    const nextBlockNewHeatLoss = lowestBlock.heatLoss + input[ nextBlockY ][ nextBlockX ];
-    const nextBlock            = blocks[ nextBlockKey ];
+input.forEach(row => {
+    console.log(row.join(''));
+});
 
-    if ( !nextBlock ) {
-        blocks[ nextBlockKey ] = {
+// ===============DEBUGGING=====================
+
+console.log(blocks[goalBlockKey].heatLoss);
+
+function getNextBlockCoords(block, direction) {
+    switch (direction) {
+        case 'north':
+            return [block.x, block.y - 1];
+        case 'east':
+            return [block.x + 1, block.y];
+        case 'south':
+            return [block.x, block.y + 1];
+        case 'west':
+            return [block.x - 1, block.y];
+    }
+}
+
+function step(lowestBlock, direction) {
+    const nextBlockCoords = getNextBlockCoords(lowestBlock, direction);
+
+    if (nextBlockCoords[0] < 0 || nextBlockCoords[0] > maxX ||
+        nextBlockCoords[1] < 0 || nextBlockCoords[1] > maxY) return;
+
+    const lowestBlockKey = `${lowestBlock.x}-${lowestBlock.y}`;
+    const nextBlockKey = `${nextBlockCoords[0]}-${nextBlockCoords[1]}`;
+    const nextBlockNewHeatLoss = lowestBlock.heatLoss + input[nextBlockCoords[1]][nextBlockCoords[0]];
+    const nextBlock = blocks[nextBlockKey];
+
+    if (!nextBlock) {
+        blocks[nextBlockKey] = {
             heatLoss: nextBlockNewHeatLoss,
             fromBlock: lowestBlockKey,
-            x: nextBlockX,
-            y: nextBlockY
+            x: nextBlockCoords[0],
+            y: nextBlockCoords[1]
         };
-    }
-    else if ( nextBlock.heatLoss > nextBlockNewHeatLoss ) {
-        nextBlock.heatLoss  = nextBlockNewHeatLoss;
+    } else if (nextBlock.heatLoss > nextBlockNewHeatLoss) {
+        nextBlock.heatLoss = nextBlockNewHeatLoss;
         nextBlock.fromBlock = lowestBlockKey;
     }
 }
 
 function getLowestHeatLossBlock() {
-    return Object.values(blocks).filter(block => !checkedBlocks.includes(`${ block.x }-${ block.y }`))
-                 .reduce((resultBlock, currentBlock) => {
-                     if ( resultBlock.heatLoss > currentBlock.heatLoss ) {
-                         return currentBlock;
-                     }
+    return Object.values(blocks).filter(block => !checkedBlocks.includes(`${block.x}-${block.y}`))
+        .reduce((resultBlock, currentBlock) => {
+            if (resultBlock.heatLoss > currentBlock.heatLoss) {
+                return currentBlock;
+            }
 
-                     return resultBlock;
-                 });
+            return resultBlock;
+        });
 }
